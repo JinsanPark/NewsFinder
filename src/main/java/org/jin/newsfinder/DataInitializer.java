1b6c326bf922f69b6586e393eca134899dc94f4d
JinsanPark <jmfa34@gmail.com>
2026-07-31 14:20:28 +0900

    검색 기능 테스트

--- src/main/java/org/jin/newsfinder/DataInitializer.java
+++ src/main/java/org/jin/newsfinder/DataInitializer.java
@@ -8,10 +8,9 @@ import org.springframework.stereotype.Component;
 import tools.jackson.databind.ObjectMapper;
 
 import java.io.File;
-import java.time.LocalDateTime;
 import java.util.List;
 
-//@Component
+@Component
 public class DataInitializer implements CommandLineRunner {
 
     private final NewsRepository newsRepository;
@@ -29,35 +28,34 @@ public class DataInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-//        NewsArticle[] articles = objectMapper.readValue(new File("data/news_articles.json"), NewsArticle[].class);
-//        int count = 0;
-//        for (NewsArticle article : articles){
-//
-//            News news = new News(
-//                    article.title(),
-//                    article.content(),
-//                    article.summary(),
-//                    article.url(),
-//                    article.source(),
-//                    article.publishedAt()
-//            );
-//
-//            List<Double> vector = embeddingClient.embedDocument(news.getSummary() + "\n" + news.getTitle());
-//            news.setEmbedding(EmbeddingConverter.toText(vector));
-//            newsRepository.save(news);
-//            count++;
-//
-//            if (count % 50 == 0){
-//                System.out.println(count + "번째 입니다.");
-//            }
-//
-//        }
+        NewsArticle[] articles = objectMapper.readValue(new File("data/news_articles_dummy.json"), NewsArticle[].class);
+        int count = 0;
+        long start = System.nanoTime();
+        for (NewsArticle article : articles){
 
-        List<NewsSearchResult> results = newsService.search("코스피 폭락");
+            News news = new News(
+                    article.title(),
+                    article.content(),
+                    article.summary(),
+                    article.url(),
+                    article.source(),
+                    article.publishedAt()
+            );
 
-        for (NewsSearchResult result : results){
-            System.out.println("유사도 점수 : " + result.score() + " | " + result.news().getTitle());
+            List<Double> vector = embeddingClient.embedDocument(news.getSummary() + "\n" + news.getTitle());
+            news.setEmbedding(EmbeddingConverter.toText(vector));
+            newsRepository.save(news);
+            count++;
+
+            if (count % 50 == 0){
+                System.out.println(count + "번째 입니다.");
+            }
+
         }
+
+        long end = System.nanoTime();
+        double seconds = (end - start) / 1_000_000_000.0;
+        System.out.printf("%.5f초%n", seconds);
 
     }
 }
